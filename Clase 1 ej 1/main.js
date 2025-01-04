5ef708cda17acb1da232167d7c936fa65a8d31d8
Sebastián Pissoni <hspisoni@gmail.com>
2025-01-04 11:17:02 -0300

    index creado

---
+++ Clase 1 ej 1/main.js
@@ -0,0 +1,28 @@
+//Variable: es un espacio de memoria reservado para almacenar informacion importante del programa
+
+
+//Declaracion
+let cliente; 
+
+
+//Asignacion de valores:
+cliente = "Carlos";
+
+cliente = "Samuel";
+
+
+console.log(cliente);
+//Inicializar variable:
+
+let  edad = 37;
+
+console.log(edad);
+
+//Const: es una variable cuyo valor no puede ser modificado una vez que ha sido asignado.
+
+const nacimiento = 1987;
+
+console.log(nacimiento);
+
+
+
---
+++ Clase 1 ejercicio2/script.js
@@ -0,0 +1,4 @@
+console.log("El script esta funcionando correctamente")
+
+const   titulo = document.getElementById ("titulo");
+titulo.textContent = "Texto  modificado"
